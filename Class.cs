d41a7f6291b0a651a060b4460cbb6c886595beb0
GritDim <gritsun.d@gmail.com>
2022-04-28 18:57:26 +0300

    Исправления внесены, добавлены свойства

---
+++ Class.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Ugol
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            set
+            {
+                if ((0 <= value) && (value <= 360))
+                {
+                    gradus = value;
+                }
+                else
+                {
+                    Console.WriteLine("Градусы угла должны быть в пределах от 0 до 360");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            get
+            {
+                return gradus;
+            }
+        }
+
+
+        public int Min
+        {
+            set
+            {
+                if ((0 <= value) && (value <= 360))
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Минуты должны быть в пределах от 0 до 60");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            get
+            {
+                return min;
+            }
+        }
+
+        public int Sec
+        {
+            set
+            {
+                if ((0 <= value) && (value <= 60))
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Секунды должны быть в пределах от 0 до 60");
+                    Console.ReadKey();
+
+                }
+            }
+
+            get
+            {
+                return sec;
+            }
+        }
+
+        public Ugol(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public void dataMesage()
+        {
+            Console.WriteLine("Задан угол {0} - градусов, {1} - минут, {2} - секунд", gradus, min, sec);
+
+        }
+
+        public void ToRadians(out double radiant)
+        {
+            radiant = (gradus + (double)min / 60 + (double)sec / 3600) * Math.PI / 180;
+            Console.WriteLine("Задан угол в радианах - {0}", radiant);
+        }
+
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -13,72 +13,15 @@ namespace Task10
             int gradus = 0;
             int min = 0;
             int sec = 0;
-            double radiant = 0;
+            double radiant;
 
             Console.WriteLine("Введите хар-ки угла");
-            Console.Write("градусов = ");
-            try
-            {
-                gradus = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
-
-
-            if ((gradus < 0) || (gradus > 360))
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.Write("минут = ");
-
-            try
-            {
-                min = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
-
-            
-            if ((min < 0) || (min > 60))
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
-
-
-            Console.Write("секунд = ");
-
-            try
-            {
-                sec = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
-
-            
-
-            if ((sec < 0) || (sec > 60))
-            {
-                Console.WriteLine("Ошибка ввода данных");
-                Console.ReadKey();
-                return;
-            }
+            Console.Write("Градусов = ");
+            gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Минут = ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Секунд = ");
+            sec = Convert.ToInt32(Console.ReadLine());
 
             Ugol ugol = new Ugol(gradus, min, sec);
 
@@ -87,7 +30,7 @@ namespace Task10
 
             Console.WriteLine();
             Console.Write("В сравнении с прямым углом заданный угол - ");
-            
+
 
             if (radiant > ((double)Math.PI / 2))
             {
@@ -110,33 +53,4 @@ namespace Task10
 
         }
     }
-
-    public class Ugol
-    {
-        public int gradus;
-        public int min;
-        public int sec;
-
-
-        public Ugol(int gradus, int min, int sec)
-        {
-            this.gradus = gradus;
-            this.min = min;
-            this.sec = sec;
-        }
-
-        public void dataMesage()
-        {
-            Console.WriteLine("Задан угол {0} - градусов, {1} - минут, {2} - секунд", gradus, min, sec);
-
-        }
-
-        public void ToRadians(out double radiant)
-        {
-            radiant = (gradus + (double)min / 60 + (double)sec / 3600) * Math.PI / 180;
-            Console.WriteLine("Задан угол в радианах - {0}", radiant);
-        }
-
-    }
-
 }
